7cf2bd9b5a9e4e6ee2ddaf6039ff1bda29f7b2ce
yasminyt <544236589@qq.com>
2018-12-12 17:49:11 +0800

    Add three sytles

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 const cssFile = [
   './css/style1/style1.css',
   './css/style2/style2.css',
-  './css/style3/style3.css'
+  './css/style3/style3.css',
+  './css/style4/style4.css',
+  './css/style5/style5.css',
+  './css/style6/style6.css',
 ]
 
 const copyright = [
   { href: 'http://www.csszengarden.com/006/', title: 'Wicked Grove', author: 'D. KEITH ROBINSON', country: 'United States'},
   { href: 'http://www.csszengarden.com/007/', title: 'Deep Thoughts', author: 'JASON ESTES', country: 'United States'},
-  { href: 'http://www.csszengarden.com/100/', title: '15 Petals', author: 'ERIC MEYER & DAVE SHEA', country: 'United States'}
+  { href: 'http://www.csszengarden.com/100/', title: '15 Petals', author: 'ERIC MEYER & DAVE SHEA', country: 'United States'},
+  { href: 'http://www.csszengarden.com/102/', title: 'Revolution!', author: 'DAVID HELLSING', country: 'Sweden'},
+  { href: 'http://www.csszengarden.com/104/', title: 'Invitation', author: 'BRAD DAILY', country: 'United States'},
+  { href: 'http://www.csszengarden.com/110/', title: 'Perfume de Gardenias', author: 'ARMANDO SOSA', country: 'Mwxico'},
+  
 ]
 
 let index = 0
